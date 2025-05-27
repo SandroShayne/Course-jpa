7f97a4e3662d25178bc0a3725b4c66857999a776
Sandro <sdutra@ciandt.com>
2025-05-27 20:11:20 -0300

    Config jpa h2

--- src/main/java/com/devsuperior/course_jpa/entities/User.java
+++ src/main/java/com/devsuperior/course_jpa/entities/User.java
@@ -3,8 +3,16 @@ package com.devsuperior.course_jpa.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
-public class User implements Serializable {
+import jakarta.persistence.*;
+;
 
+
+@Entity
+@Table(name ="tb_user")
+public class User implements Serializable{
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String name;
     private String email;
