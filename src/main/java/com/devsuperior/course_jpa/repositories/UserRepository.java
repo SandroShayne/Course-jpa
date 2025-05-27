7c0ec770c85f03bde95dc5c06feef761a9abc2d9
Sandro <sdutra@ciandt.com>
2025-05-27 20:36:57 -0300

    Criacao JpaRepositoty

---
+++ src/main/java/com/devsuperior/course_jpa/repositories/UserRepository.java
@@ -0,0 +1,8 @@
+package com.devsuperior.course_jpa.repositories;
+
+import com.devsuperior.course_jpa.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
