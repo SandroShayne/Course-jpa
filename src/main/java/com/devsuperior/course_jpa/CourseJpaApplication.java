2f28850d7133ee29fc12f74e1822c61f71ca272a
Sandro <sdutra@ciandt.com>
2025-05-27 13:42:11 -0300

    project course

---
+++ src/main/java/com/devsuperior/course_jpa/CourseJpaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.course_jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CourseJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseJpaApplication.class, args);
+	}
+
+}
